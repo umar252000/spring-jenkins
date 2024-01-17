8f7552a02a0acd1fb5ff2dc7454cc2f775af36f7
umar252000 <mansuriumar86436@gmail.com>
2024-01-17 20:33:51 +0530

    Api

--- src/main/java/com/umar/SpringJenkinsApplication.java
+++ src/main/java/com/umar/SpringJenkinsApplication.java
@@ -6,9 +6,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.annotation.PostConstruct;
-
+@RestController
 @SpringBootApplication
 public class SpringJenkinsApplication {
 
